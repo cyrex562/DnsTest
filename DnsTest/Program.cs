5fe38bcde103d6a35785cd4cd436052dde89c3a5
Josh Madden <jomadde@microsoft.com>
2018-04-23 10:28:29 -0400

    adding wrapper library for wpcap

--- DnsTest/Program.cs
+++ DnsTest/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,8 @@ namespace DnsTest
     {
         static void Main(string[] args)
         {
+            Debug.WriteLine("Main()");
+
         }
     }
 }
